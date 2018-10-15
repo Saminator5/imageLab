6b4a761e02c5fab2f98e4f5020fbbe98b715d329
John Doe <slefcourt12@gmail.com>
2018-10-14 23:22:43 -0500

    combined module a and module b.

--- ImageLab/ImageLab-Bridging-Header.h
+++ ImageLab/ImageLab-Bridging-Header.h
@@ -2,4 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "OpenCVBridgeSub.h"
+#import "OpenCVBridgeSub2.h"
---
+++ ImageLab/OpenCVBridgeSub.swift
@@ -0,0 +1,9 @@
+//
+//  OpenCVBridgeSub.swift
+//  ImageLab
+//
+//  Created by Samuel Lefcourt on 10/14/18.
+//  Copyright © 2018 Eric Larson. All rights reserved.
+//
+
+import Foundation
